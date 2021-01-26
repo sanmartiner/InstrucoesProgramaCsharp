d1fb06109bb64369ab814e7a687c22cf2debca85
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2021-01-26 16:29:36 -0400

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Instrucoes
+{
+    class Program
+    {
+        static void Declaracoes()
+        {
+            int a;
+            int b = 2, c = 3;
+            const int d = 4;
+            a = 1;
+            Console.WriteLine(a + b + c + d);
+        }
+        
+        static void InstrucaoIf(string[] args) 
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Nenhum Argumento");
+            } 
+            else if{
+                Console.WriteLine("Um argumento");
+            }
+            else
+            {
+                Console.WriteLine($"{args.Length} argumentos");
+            }
+        }
+
+        static void InstrucaoSwitch(string[] args)
+        { }
+
+        static void InstrucaoDo(string[] args)
+        { }
+
+        static void InstrucaoFor(string[] args)
+        { }
+
+        
+    }
+}
